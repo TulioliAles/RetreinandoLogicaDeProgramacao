bb6a8a55c69fe98357539933fd4a3f9467c971d4
TulioliAles <desmecad@hotmail.com>
2020-09-19 13:09:54 -0300

    Commit de exercicios de lógica

---
+++ CalculaVolumeEsfera/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalculaVolumeEsfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, volume = 0;
+            Console.WriteLine("==== Programa que Calcula o Volume de uma Esfera ====");
+            Console.Write("Infrome o Raio da Esfera: ");
+            raio = Convert.ToDouble(Console.ReadLine());
+            volume = (4 / 3.0) * 3.14159 * Math.Pow(raio, 3);
+            Console.WriteLine("VOLUME = " + Math.Round(volume, 3));
+        }
+    }
+}
