5da921136c06a432cfca6aee5a945a3db8b2d396
TulioliAles <desmecad@hotmail.com>
2020-09-19 13:39:35 -0300

    Commit de exercicios de lógica

---
+++ CalculaConsumoCombustivel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalculaConsumoCombustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal km, ltr, consumo = 0;
+            Console.WriteLine("==== Programa para Cálculo de Consumo de Combustível ==== ");
+            
+            Console.Write("Informe o valor de Km rodado: ");
+            km = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Informe o valor de Litros consumidos: ");
+            ltr = Convert.ToDecimal(Console.ReadLine());
+
+            consumo = (km / ltr) * 10;
+
+            Console.WriteLine(Math.Round(consumo, 3) + " km/l");
+        }
+    }
+}
